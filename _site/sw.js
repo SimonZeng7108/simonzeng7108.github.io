7825988600c2d18c0d10e36b7cd0207b82da3fa2
Simon Zeng <simon7108528@gmail.com>
2024-02-13 14:45:39 +0000

    update ruby version

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240213.143217';
+const cacheName = 'chirpy-20240213.144440';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
