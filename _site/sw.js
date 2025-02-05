e30edf9084378991948bd20052b422b9cdc07933
Simon Zeng <simon7108528@gmail.com>
2025-02-05 15:41:38 +0000

    artifacts

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250205.152028';
+const cacheName = 'chirpy-20250205.153850';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
